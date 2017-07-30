f81c3a85e8b33190d3449da4175987ff6ac21d20
Raunak <raunakagrawal1001@gmail.com>
2017-07-30 18:57:30 +0530

    bash file added

---
+++ script.sh
@@ -0,0 +1,9 @@
+sudo apt update
+sudo apt upgrade    
+sudo apt-get install docky
+sudo apt-get install gedit
+sudo apt install openvpn
+sudo apt install git
+sudo apt install zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
